6c3fd5f99a5f5e2fbbfccac81934807018cca2a2
louis Turpault <louis.turpault@gmail.com>
2025-10-05 12:59:27 -0400

    added pid saving

--- src/backend/start.sh
+++ src/backend/start.sh
@@ -3,4 +3,5 @@ cd /home/serge/prog/nasa-space-apps-2025/src/backend
 git stash
 git stash drop
 git pull
-gunicorn -w 4 -b 0.0.0.0:8080  wsgi:app --error-logfile error.log --access-logfile access.log --capture-output --log-level debug
+
+/home/serge/prog/nasa-space-apps-2025/.venv/bin/gunicorn -w 4 -b 0.0.0.0:8080  wsgi:app --error-logfile error.log --access-logfile access.log --capture-output --log-level debug --pid /run/gunicorn.pid
