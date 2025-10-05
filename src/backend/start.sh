e5ced97eb0614c87553def55107ee3194959d145
louis Turpault <louis.turpault@gmail.com>
2025-10-05 12:48:06 -0400

    start work on service files

---
+++ src/backend/start.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+cd /home/serge/prog/nasa-space-apps-2025/src/backend
+git stash
+git stash drop
+git pull
+gunicorn -w 4 -b 0.0.0.0:8080  wsgi:app --error-logfile error.log --access-logfile access.log --capture-output --log-level debug
