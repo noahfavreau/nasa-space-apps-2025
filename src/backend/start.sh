02573c0c40dcabbbaf1cd54928c9308e00037227
louis Turpault <louis.turpault@gmail.com>
2025-10-05 14:18:14 -0400

    bootloop fix!

--- src/backend/start.sh
+++ src/backend/start.sh
@@ -7,4 +7,4 @@ git pull
 
 
 echo "starting Gunicorn"
-/home/serge/prog/nasa-space-apps-2025/.venv/bin/gunicorn -w 4 -b 0.0.0.0:8080  wsgi:app --error-logfile error.log --access-logfile access.log --capture-output --log-level debug --pid /home/serge/.gunicorn.pid
+/home/serge/prog/nasa-space-apps-2025/.venv/bin/gunicorn -w 4 -b 0.0.0.0:8080  wsgi:app --error-logfile /home/serge/error.log --access-logfile /home/serge/access.log --capture-output --log-level debug --pid /home/serge/.gunicorn.pid
