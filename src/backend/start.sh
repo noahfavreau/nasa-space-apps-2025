0fc12a200955485144687d529629b71aa97d3da7
louis Turpault <louis.turpault@gmail.com>
2025-10-05 13:18:22 -0400

    finished service

--- src/backend/start.sh
+++ src/backend/start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 cd /home/serge/prog/nasa-space-apps-2025/src/backend
-rm /run/gunicorn.pid
+rm /home/serge/.gunicorn.pid
 git stash
 git stash drop
 git pull
 
 
 echo "starting Gunicorn"
-/home/serge/prog/nasa-space-apps-2025/.venv/bin/gunicorn -w 4 -b 0.0.0.0:8080  wsgi:app --error-logfile error.log --access-logfile access.log --capture-output --log-level debug --pid /run/gunicorn.pid
+/home/serge/prog/nasa-space-apps-2025/.venv/bin/gunicorn -w 4 -b 0.0.0.0:8080  wsgi:app --error-logfile error.log --access-logfile access.log --capture-output --log-level debug --pid /home/serge/.gunicorn.pid
