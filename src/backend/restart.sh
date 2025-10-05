cf38dcb9d7edb38fcc9bd1e524f8c1227436a879
louis Turpault <louis.turpault@gmail.com>
2025-10-05 16:05:37 -0400

    bruh will it ever work

--- src/backend/restart.sh
+++ src/backend/restart.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 git fetch origin
-A= $(git log -1 --format=%ci origin/main)
+A= git log -1 --format=%ci origin/main
 Current= $(cat current.txt)
 if [$((A-current)) > 0]; then
     systemctl restart gunicorn_backend.service
